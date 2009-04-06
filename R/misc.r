9223e7c134ab081ca1d16adecce51fc4a82808c2
stat <stat@8193cdef-c62e-4131-a968-7dcc8d47c1f7>
2009-04-06 14:27:22 +0000

    karsten: two versions for spherical harmonics handling
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/dti-svn/trunk@222 8193cdef-c62e-4131-a968-7dcc8d47c1f7

--- R/misc.r
+++ R/misc.r
@@ -263,65 +263,6 @@ dim(mask1) <- dm
 mask1
 }
 
-getsphericalharmonicseven <- function(order,theta,phi){
-#
-#   compute spherical harmonics
-#
-require(gsl)
-order <- as.integer(max(0,order))
-if(order%%2==1){
-warning("maximum order needs to be even, increase order by one")
-order <- order+1
-} 
-if(length(theta)!=length(phi)) stop("need same length of theta and phi")
-kseq <- seq(0,order,2)
-n <- length(phi)
-values <- matrix(0,(order+1)*(order+2)/2,n)
-for(k in kseq){
-mseq <- seq(-k,k,1)
-for(m in mseq){
-ind <- (k^2+k+2)/2+m
-z <- legendre_sphPlm(k,abs(m),cos(theta))
-if(m < 0){
-z <- sqrt(2)*z*cos(m*phi)
-} 
-if(m > 0){
-z <- sqrt(2)*z*sin(m*phi)
-}
-values[ind,] <- z
-}
-}
-values
-}
-
-getsphericalharmonicsall <- function(order,theta,phi){
-#
-#   compute spherical harmonics
-#
-require(gsl)
-order <- as.integer(max(0,order))
-if(length(theta)!=length(phi)) stop("need same length of theta and phi")
-kseq <- 0:order
-n <- length(phi)
-values <- matrix(0,(order+1)^2,n)
-l <- 1
-for(k in kseq){
-mseq <- (-k):k
-for(m in mseq){
-z <- legendre_sphPlm(k,abs(m),cos(theta))
-if(m < 0){
-z <- sqrt(2)*z*cos(m*phi)
-} 
-if(m > 0){
-z <- sqrt(2)*z*sin(m*phi)
-}
-values[l,] <- z
-l <- l+1
-}
-}
-values
-}
-
 sphcoord <- function(ccoord){
 #
 #  transform cartesian into sherical coordinates
---
+++ R/windows/spherharm.r
@@ -0,0 +1,13 @@
+getsphericalharmonicseven <- function(order,theta,phi){
+#
+#   compute spherical harmonics
+#
+stop("GNU Scientific library needed for this function! Not implemented for Windows! Sorry!")
+}
+
+getsphericalharmonicsall <- function(order,theta,phi){
+#
+#   compute spherical harmonics
+#
+stop("GNU Scientific library needed for this function! Not implemented for Windows! Sorry!")
+}
