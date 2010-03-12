794e9cf961f3ec0d6bfa68961a392b77cde3d8b7
stat <stat@8193cdef-c62e-4131-a968-7dcc8d47c1f7>
2010-03-12 10:34:36 +0000

    version der mixtensor in C. diese version laeuft, es ist aber nicht getestet, ob sie sinnvolle resultate liefert.
    ausserdem ist sie viel zu langsam, zT langsamer als die alte version ->
    code-optimierung!
    
    beachte auch, dass die alte version ueber dwiMixtensor.old() erreichbar
    ist (NAMESPACE), allerdings dort auch mit einem voxelweisen direkten
    C-Aufruf der Optimierung.
    
    karsten
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/dti-svn/trunk@300 8193cdef-c62e-4131-a968-7dcc8d47c1f7

--- R/mixtensor.r
+++ R/mixtensor.r
@@ -4,41 +4,65 @@
 #  Misc. functions
 #
 
-neighbors <- function(grad,m=7){
-ngrad <- dim(grad)[1]
-z <- apply(as.matrix(dist(rbind(grad,-grad)))[1:ngrad,],1,order)[1:m,]
-# also consider opposite directions
-z[z>ngrad] <- z[z>ngrad]-ngrad
-# identify opposite directions
-z
+neighbors <- function(grad, m=7) {
+  ngrad <- dim(grad)[1]
+  z <- apply(as.matrix(dist(rbind(grad,-grad)))[1:ngrad,],1,order)[1:m,]
+  # also consider opposite directions
+  z[z>ngrad] <- z[z>ngrad]-ngrad
+  # identify opposite directions
+  z
 }
 
 locmin <- function(si,sneighbors,grad){
-ngrad <- length(si)
-z <- si[sneighbors]
-dim(z) <- dim(sneighbors)
-ind <- (1:ngrad)[apply(t(z)>=si,1,all)]
-ind <- ind[order(si[ind])]
-list(ind = ind, si = si[ind], grad = grad[ind,,drop=FALSE])
+  ngrad <- length(si)
+  z <- si[sneighbors]
+  dim(z) <- dim(sneighbors)
+  ind <- (1:ngrad)[apply(t(z)>=si,1,all)]
+  ind <- ind[order(si[ind])]
+  list(ind = ind, si = si[ind], grad = grad[ind,,drop=FALSE])
 }
 
+locmin2 <- function(si, sneighbors){
+  ddim <- dim(si)[1:3]
+  ngrad <- dim(si)[4]
+  si <- .C("smoothsi",
+           as.integer(ddim[1]),
+           as.integer(ddim[2]),
+           as.integer(ddim[3]),
+           as.double(si),
+           as.integer(ngrad),
+           as.integer(sneighbors),
+           as.integer(dim(sneighbors)[1]),
+           si = double(length(si)),
+           PACKAGE = "dti")$si
+  dim(si) <- c(ddim,ngrad)
+  minsi <- function(x, sneighbors) {
+    z <- x[sneighbors]
+    dim(z) <- dim(sneighbors)
+    ind <- (1:ngrad)[apply(t(z) >= x, 1, all)]
+    si2 <- rep(1, dim(sneighbors)[2])
+    si2[ind] <- x[ind]
+    si2
+  }
+  s <- apply(si, 1:3, minsi, sneighbors)
+}
 
 paroforient <- function(dir){
-   theta <- acos(dir[3])
-   sth <- sin(theta)
-   phi <- 0
-   if(sth<1e-8){
-      theta <- 0
-   } else {
-      z <- dir[1]/sth
-      if(abs(z)>=1){
+  theta <- acos(dir[3])
+  sth <- sin(theta)
+  phi <- 0
+  if(sth<1e-8) {
+    theta <- 0
+  } else {
+    z <- dir[1]/sth
+    if(abs(z)>=1) {
       phi <- if(z<0) 0 else pi
-      } else {
+    } else {
       phi <- acos(z)*sign(dir[2])
-      }
-      if(phi < 0) phi <- phi+2*pi
-   }
-c(theta,phi)
+    }
+    if(phi < 0) phi <- phi+2*pi
+  }
+  c(theta, phi)
 }
 
 getw <- function(cw){
@@ -100,11 +124,11 @@ ep <- max(1,min(par[lpar],50))
 }
 
 
-dwiMixtensor <- function(object, ...) cat("No dwiMixtensor calculation defined for this class:",class(object),"\n")
+dwiMixtensor.old <- function(object, ...) cat("No dwiMixtensor.old calculation defined for this class:",class(object),"\n")
 
-setGeneric("dwiMixtensor", function(object,  ...) standardGeneric("dwiMixtensor"))
+setGeneric("dwiMixtensor.old", function(object,  ...) standardGeneric("dwiMixtensor.old"))
 
-setMethod("dwiMixtensor","dtiData",function(object, maxcomp=2, p=40, maxneighb=7, method="mixtensor", reltol=1e-8, maxit=5000){
+setMethod("dwiMixtensor.old","dtiData",function(object, maxcomp=2, p=40, maxneighb=7, method="mixtensor", reltol=1e-8, maxit=5000){
   args <- sys.call(-1)
   args <- c(object@call,args)
   ngrad <- object@ngrad
@@ -195,7 +219,7 @@ setMethod("dwiMixtensor","dtiData",function(object, maxcomp=2, p=40, maxneighb=7
                                    as.double(reltol),
                                    value = as.double(1))[c("par", "value")],
                  "Jian"       = optim(par[1:(3*k+1)],mfun2,siq=siq[i1,i2,i3,],grad=grad,ep=p,control=list(maxit=maxit,reltol=reltol)),
-                 "Jian2"      = optim(c(par[1:(3*k+1)],25),mfun3,siq=siq[i1,i2,i3,],grad=grad,ep=p,control=list(maxit=maxit,reltol=reltol)))
+                 "Jian2"      = optim(c(par[1:(3*k+1)],25),mfun3,siq=siq[i1,i2,i3,],grad=grad,control=list(maxit=maxit,reltol=reltol)))
         rss <- min(z$value,rss)
         ttt <- z$value+2*(3*k+1)/(ngrad-3*maxcomp-1)*rss
 #        cat("risk",z$value,ttt,"\n")
@@ -253,6 +277,138 @@ setMethod("dwiMixtensor","dtiData",function(object, maxcomp=2, p=40, maxneighb=7
                 outlier = index,
                 scale = 1,
                 method = method)
+            )
+}
+)
+
+dwiMixtensor <- function(object, ...) cat("No dwiMixtensor calculation defined for this class:",class(object),"\n")
+
+setGeneric("dwiMixtensor", function(object,  ...) standardGeneric("dwiMixtensor"))
+
+setMethod("dwiMixtensor", "dtiData", function(object, maxcomp=2, p=2, maxneighb=7, method="mixtensor", reltol=1e-8, maxit=5000) {
+
+  cat("entering method dwiMixtensor\n")
+  args <- sys.call(-1)
+  args <- c(object@call, args)
+  ngrad <- object@ngrad
+  ddim <- object@ddim
+  s0ind <- object@s0ind
+  ns0 <- length(s0ind)
+
+  cat("determine outliers ... ")
+  # determine outlier ????
+  z <- .Fortran("outlier",
+                as.integer(object@si),
+                as.integer(prod(ddim)),
+                as.integer(ngrad),
+                as.logical((1:ngrad)%in%s0ind),
+                as.integer(ns0),
+                si      = integer(prod(ddim)*ngrad),
+                index   = integer(prod(ddim)),
+                lindex  = integer(1),
+                DUPL    = FALSE,
+                PACKAGE = "dti")[c("si","index","lindex")]
+  si <- array(z$si, c(ddim, ngrad))
+  index <- if (z$lindex>0) z$index[1:z$lindex] else numeric(0)
+  rm(z)
+  cat("done\n")
+
+  cat("prepare initial estimates ... ")
+  # prepare data for optim
+  s0 <- si[,,,s0ind]
+  si <- si[,,,-s0ind]
+  if (length(s0ind)>1) s0 <- apply(s0, 1:3, mean)
+  # normalized DW data
+  siq <- sweep(si,1:3,s0,"/")
+  # heuristics to avoid DWI that are larger than s0.
+  siqmed <- apply(siq,1:3,median)
+  siqmed[siqmed<.9] <- .9
+  siqmed[siqmed>.99] <- .99
+  siq <- sweep(siq,1:3,siqmed,pmin)
+  # mask for calculation
+  mask <- s0 > 0
+  grad <- t(object@gradient[,-s0ind])
+  sneighbors <- neighbors(grad, maxneighb)
+  s1 <- locmin2(siq, sneighbors) # NOTE! dim(s1) == c(ngrad-length(s0ind), ddim)
+  ordersi <- function(x, ncomp) order(x)[1:ncomp]
+  siind <- numeric( (maxcomp + 1) * prod(ddim))
+  dim(siind) <- c( maxcomp + 1, ddim)
+  siind[-1,,,] <- apply(s1, 2:4, ordersi, maxcomp)
+  countmin <- function(x, ncomp) min(sum(x < 1), ncomp)
+  siind[1,,,] <- apply(s1, 2:4, countmin, maxcomp)
+  cat("done\n")
+
+  cat("optimizing ... ")
+  mm <- switch(method, "mixtensor" = 1,
+                       "Jian"      = 2,
+#                       "Jian2"     = 3,
+                       1)
+  pl <- if (method == "Jian2") prod(ddim) else 1;
+
+  # perform voxelwise optimization and order selection of tensor mixture model
+  a <- .C("mixture",
+          as.integer(mm),                         # select mixture method
+          as.integer(ddim[1]),                    # voxel dim x
+          as.integer(ddim[2]),                    # voxel dim y
+          as.integer(ddim[3]),                    # voxel dim z
+          as.integer(mask),                       # calculation mask
+          as.double(siq),                         # DWI without s0
+          as.integer(siind),                      # DWI indices of local minima
+          as.integer(ngrad - length(s0ind)),      # number of DWI
+          as.double(grad),                        # gradient directions
+          as.integer(sneighbors),                 # (maxneighb x ngrad) matrix of nearest gradient neighbors
+          as.integer(maxcomp),                  # max number of gradient neighbors
+          as.integer(pl),                         # exp for Jian model
+          as.integer(maxit),                      # max number of iterations for optim
+          as.double(reltol),                      # reltol crit for optim
+          order   = double(prod(ddim)),   # selected order of mixture
+          lev     = double(2*prod(ddim)),         # logarithmic eigenvalues
+          mix     = double(maxcomp*prod(ddim)),   # mixture weights
+          orient  = double(2*maxcomp*prod(ddim)), # phi/theta for all mixture tensors
+          p       = double(pl),                   # decay const in Jian
+          sigma2  = double(prod(ddim)),           # parameter variance ???
+          PACKAGE = "dti")[c("order", "lev", "mix", "orient", "p", "sigma2")]
+
+  # set dimension attr
+  dim(a$order) <- ddim;
+  dim(a$lev) <- c(2, ddim);
+  dim(a$mix) <- c(maxcomp, ddim);
+  dim(a$orient) <- c(2, maxcomp, ddim);
+  if (method == "Jian2") dim(a$p) <- ddim;
+  dim(a$sigma2) <- ddim;
+  cat("done\n")
+
+  # create and return new object
+  invisible(new("dwiMixtensor",
+                call        = args,
+                ev          = exp(a$lev),
+                mix         = a$mix,
+                orient      = a$orient,
+                order       = a$order,
+                p           = if (method == "Jian2") a$p else p,
+                th0         = s0,
+                sigma       = a$sigma2,
+                scorr       = array(1, c(1,1,1)), # ???
+                bw          = c(0,0,0),           # ???
+                mask        = mask,
+                hmax        = 1,                  # ???
+                gradient    = object@gradient,
+                btb         = object@btb,
+                ngrad       = object@ngrad,
+                s0ind       = object@s0ind,
+                replind     = object@replind,
+                ddim        = object@ddim,
+                ddim0       = object@ddim0,
+                xind        = object@xind,
+                yind        = object@yind,
+                zind        = object@zind,
+                voxelext    = object@voxelext,
+                level       = object@level,
+                orientation = object@orientation,
+                source      = object@source,
+                outlier     = index,
+                scale       = 1,
+                method      = method)
             )
 }
 )
--- src/mixtensor.c
+++ src/mixtensor.c
@@ -3,31 +3,164 @@
 #include <Rmath.h>
 #include <R_ext/Applic.h>
 
-typedef struct {
+typedef struct
+{
   int ngrad;
   double *siq;
   double *grad;
+  int p;
 } optimex;
 
 // double F77_NAME(dotprod3)(double *, double *);
+// double z1 = F77_CALL(dotprod3)(dir, test);
 
-double fn1(int n, double *par, void *ex) {
+void paroforient(double *dir, double *angles)
+{
+  angles[0] = acos(dir[2]);
+  double sth = sin(angles[0]);
+  angles[1] = 0.;
+
+  if (sth < 1e-8)
+  {
+    angles[0] = 0.;
+  }
+  else
+  {
+    double z = dir[0]/sth;
+    angles[1] = (abs(z) >= 1.) ? ((z<0.) ? 0. : M_PI) : acos(z)*sign(dir[1]);
+    if (angles[1] < 0.) angles[1] += M_2PI;
+  }
+  return;
+}
+
+inline int get_ind2d(int i, int j, int n1)
+{
+  return i + j*n1;
+}
+
+inline int get_ind3d(int i, int j, int k, int n1, int n2)
+{
+  return i + j*n1 + k*n1*n2;
+}
+
+inline int get_ind4d(int i, int j, int k, int l, int n1, int n2, int n3)
+{
+  return i + j*n1 + k*n1*n2 + l*n1*n2*n3;
+}
+
+inline int get_ind5d(int i, int j, int k, int l, int m, int n1, int n2, int n3, int n4)
+{
+  return i + j*n1 + k*n1*n2 + l*n1*n2*n3 + m*n1*n2*n3*n4;
+}
+
+void smoothsi(int *r1, int *r2, int *r3, double *si, int *n, int *sneighbors, int *nneighbors, double *smoothedsi)
+{
+  int n1 = *r1, n2 = *r2, n3 = *r3, ngrad = *n, nn = *nneighbors;
+  double w[nn];
+  int i1, i2, i3, i, j;
+  double sw = 0.;
+  double z;
+
+  for (j = 0; j < nn; j++)
+  {
+    w[j] = 1./(j+1.);
+    sw += w[j];
+  }
+
+  for (i1 = 0; i1 < n1; i1++)
+  {
+    for (i2 = 0; i2 < n2; i2++)
+    {
+      for (i3 = 0; i3 < n3; i3++)
+      {
+        for (i = 0; i < ngrad; i++)
+        {
+          z = 0.;
+          for (j = 0; j < nn; j++) 
+            z += w[j]*si[get_ind4d(i1, i2, i3, sneighbors[get_ind2d(j, i, nn)]-1, n1, n2, n3)];
+          smoothedsi[get_ind4d(i1, i2, i3, i, n1, n2, n3)] = z/sw;
+        }
+      }
+    }
+  }
+  return;
+}
+
+void getweights(int m, double *cw, double *z)
+{
+  double ew;
+  double sw = 0;
+
+  for (int i = 0; i < m; i++)
+  {
+    // use log and exp to have free optim problem
+    ew = exp(cw[i]);
+    z[i] = (1 - sw) * ew/(1+ew);
+    sw += z[i];
+  }
+  z[m] = 1 - sw;
+  return;
+}
+
+double fn1(int n, double *par, void *ex)
+{
+  optimex ext = *((optimex*)ex);
+
+  int m = (n-1)/3;     // order of mix tensor model
+  double z[ext.ngrad]; // value of signal fct. for each grad according to current model
+  for (int k = 0; k < ext.ngrad; k++)
+    z[k] = 0;
+  double dir[3];       // used for direction vector calculated from angles in par
+  double c1 = exp(par[0]), c2 = exp(par[1]), sw = 0, w, ew, sth, z1 = 0;
+  int i, j;
+
+  double erg = 0; // result
+
+  for (i = 0; i < m; i++)
+  {
+    if (i == m-1 )
+    {
+      w = 1.0 - sw;
+    }
+    else
+    {
+      // use log and exp to have free optim problem
+      ew = exp(par[3*i+4]);
+      w = (1.0 - sw)*ew/(1.0+ew);
+      sw += w;
+    }
+    sth = sin(par[i*3+2]);
+    dir[0] = sth*cos(par[i*3+3]);
+    dir[1] = sth*sin(par[i*3+3]);
+    dir[2] = cos(par[i*3+2]);
+    for (j = 0; j < ext.ngrad; j++)
+    {
+      z1 = dir[0]*ext.grad[0+j] + dir[1]*ext.grad[ext.ngrad+j] + dir[2]*ext.grad[2*ext.ngrad+j]; // CHECK INDEX!!!!!!!!!!!!!!!!!!!!!!
+      z[j] += (ext.p == 0) ? w*exp(-c2 - c1*z1*z1) : w*exp(-ext.p*log(1 + ( c2 + c1*z1*z1 )/ext.p));
+    }
+  }
+
+  // calculate residual variance
+  for (j = 0; j < ext.ngrad; j++)
+  {
+    z1 = ext.siq[j] - z[j];
+    erg += z1*z1;
+  }
+
+  // finished
+  return erg;
+}
+
+/*
+double fn2(int n, double *par, void *ex) {
   optimex ext = *((optimex*)ex);
 
   int m = (n-1)/3; // order of mix tensor model
-
   double z[ext.ngrad]; // value of signal fct. for each grad according to current model
   for (int k = 0; k < ext.ngrad; k++) z[k] = 0;
   double dir[3]; // used for direction vector calculated from angles in par
-  double c1 = exp(par[0]);
-  double c2 = exp(par[1]);
-  double sw = 0;
-  double w;
-  double ew;
-  double sth;
-  double z1 = 0;
-  int i;
-  int j;
+  double c1 = exp(par[0]), c2 = exp(par[1]), sw = 0, w, ew, sth, z1 = 0;
+  int i, j;
 
   double erg = 0; // result
 
@@ -45,8 +178,7 @@ double fn1(int n, double *par, void *ex) {
     dir[2] = cos(par[i*3+2]);
     for (j = 0; j < ext.ngrad; j++) {
       z1 = dir[0]*ext.grad[0+j] + dir[1]*ext.grad[ext.ngrad+j] + dir[2]*ext.grad[2*ext.ngrad+j];
-//      z1 = F77_CALL(dotprod3)(dir, test);
-      z[j] += w*exp(-c2 - c1*z1*z1);
+      z[j] += w*exp(-par[n-1]*log(1 + ( c2 + c1*z1*z1 )/par[n-1]));
     }
   }
 
@@ -59,91 +191,166 @@ double fn1(int n, double *par, void *ex) {
   // finished
   return erg;
 }
+*/
 
 void mixtensor(int *n, double *par, double *x, int *ngrad, double *siq, double *grad, int *maxit, double *reltol, double *Fmin){
 
-  int fail; // failure code: zero is OK
-  int fncount; // number of calls to fct.
-
+  int fail, fncount; // failure code: zero is OK, number of calls to fct.
   optimex myoptimpar;
+
   myoptimpar.ngrad = *ngrad;
   myoptimpar.siq = siq;
   myoptimpar.grad = grad;
+  myoptimpar.p = 0;
 
   nmmin(*n, par, x, Fmin, fn1,
         &fail, R_NegInf, *reltol, &myoptimpar,
         1.0, 0.5, 2.0, 0,
         &fncount, *maxit);
-
+  return;
 }
-/*
-void mixture(int *n1, int *n2, int *n3, double *mask, double *si, int *ngrad, double *grad, int *sneighbors, int *maxneighb, double *sweights, int *maxcomp, int *maxit, double *reltol,
+
+void mixture(int *method, int *r1, int *r2, int *r3, int *mask, double *siq, int *siind, int *n, double *grad, int *sneighbors, int *maxcomp, int *ep, int *maxit, double *reltol,
              double *order, double *lev, double *mix, double *orient, double *p, double *sigma2){
 
-  int i1;
-  int i2;
-  int i3;
-  int mc0;
-  // z;
-  double *par;
-  int lpar;
-  double rss = R_PosInf;
-  double krit = R_NegInf;
-  double *siq;
-  double *smsi;
+  int n1 = *r1, n2 = *r2, n3 = *r3, ngrad = *n, mc = *maxcomp;
+  Rprintf("entering %i %i %i %i\n", n1, n2, n3, ngrad);
+  int i1, i2, i3, mc0, lpar;
+  double rss, krit, ttt;
+  double siiq[ngrad];
+  int fail;              // failure code for optim: zero is OK
+  int fncount;           // number of calls to obj fct in optim
+  double Fmin = 0.;          // minimal value of obj fct in optim
+  int i, k;
+  int gradind;
+  double angles[2];
+  double dir[3];
 
-  for (int i1 = 0; i1 < n1; i1++) {
-    for (int i2 = 0; i2 < n2; i2++) {
-      for (int i3 = 0; i3 < n3; i3++) {
-        if mask[i1][i2][i3] break; // formuliere als eindim. array! und wir wollen kein break!
-        smsi = F77_CALL(smsi)(si[i1][i2][i3][], *ngrad, *sneighbors, *maxneighb, *sweights); // definiere in C und als function!
 
-        z <- locmin(smsi,sneighbors,grad) // wie loesen?
-        mc0 = min(*maxcomp, dim(z$grad)[1]) // ueber if
+  for (i1 = 0; i1 < n1; i1++)
+  {
+    for (i2 = 0; i2 < n2; i2++)
+    {
+      for (i3 = 0; i3 < n3; i3++)
+      {
+        Rprintf("iterating %i %i %i\n", i1, i2, i3);
+        if (mask[get_ind3d(i1, i2, i3, n1, n2)] != 1)
+        {
+          order[get_ind3d(i1, i2, i3, n1, n2)] = 0;
+          lev[get_ind4d(0, i1, i2, i3, 2, n1, n2)] = 0;
+          lev[get_ind4d(1, i1, i2, i3, 2, n1, n2)] = 0;
+          for (i = 0; i < mc; i++)
+          {
+            mix[get_ind4d(i, i1, i2, i3, mc, n1, n2)] = 0;
+            orient[get_ind5d(0, i, i1, i2, i3, 2, mc, n1, n2)] = 0;
+            orient[get_ind5d(1, i, i1, i2, i3, 2, mc, n1, n2)] = 0;
+          }
+//          if (*method == 3) p[get_ind3d(i1, i2, i3, n1, n2)] = 0;
+          sigma2[get_ind3d(i1, i2, i3, n1, n2)] = 0;
+        }
+        else
+        {
+          // prepare signal
+          for (i = 0; i < ngrad; i++)
+          {
+            siiq[i] = siq[get_ind4d(i1, i2, i3, i, n1, n2, n3)];
+          }
+          // determine useful prime estimates
+          mc0 = siind[get_ind4d(0, i1, i2, i3, mc+1, n1, n2)]; // order of modell 
+          lpar = (*method == 3) ? 3*mc0+2 : 3*mc0+1;
+          double par[lpar];
+          double x[lpar];
+          gradind = siind[get_ind4d(1, i1, i2, i3, mc+1, n1, n2)];
+          par[0] = log(-log(siq[get_ind4d(i1, i2, i3, gradind-1, n1, n2, n3)])*.8);
+          par[1] = log(-log(siq[get_ind4d(i1, i2, i3, gradind-1, n1, n2, n3)])*.2);
+          for (i = 0; i < mc0; i++)
+          {
+            gradind = siind[get_ind4d(i+1, i1, i2, i3, mc+1, n1, n2)];
+            dir[0] = grad[get_ind2d(gradind-1, 0, ngrad)];
+            dir[1] = grad[get_ind2d(gradind-1, 1, ngrad)];
+            dir[2] = grad[get_ind2d(gradind-1, 2, ngrad)];
+            paroforient(dir, angles);    // INDEX????
+            par[3*i + 2] = angles[0];
+            par[3*i + 3] = angles[1]; 
+//            Rprintf("par[] %i, %f %f\n", 3*i+2, par[3*i+2], par[3*i+3]); 
+          }
+//         if (*method == 3) par[lpar] = 0;
 
-        for(j in 1:mc0) orient[,j,i1,i2,i3] <- paroforient(z$grad[j,])
-        par <- numeric(3*mc0+1)
-        lpar = 3*mc0+1
-        lev[,i1,i2,i3] <- par[1:2] <- log(-log(siq[i1,i2,i3,z$ind[1]])*c(.8,.2))
-        par[rep(3*(1:mc0),rep(2,mc0))+c(0,1)] <- orient[,1:mc0,i1,i2,i3] 
-        rss <- Inf
-        krit <- Inf
-        for(k in mc0:1){
-          if(k>1) par[3*(2:k)-1] <- -log((k-1):1)
-          lpar <- length(par);
-        z <-switch(method,
-                  "mixtensor" = .C("mixtensor",
-                                   as.integer(lpar),
-                                   as.double(par),
-                                   par = double(lpar),
-                                   as.integer(ngrad0),
-                                   as.double(siq[i1,i2,i3,]),
-                                   as.double(as.matrix(grad)),
-                                   as.integer(maxit),
-                                   as.double(reltol),
-                                   value = as.double(1))[c("par", "value")],
-                 "Jian"       = optim(par[1:(3*k+1)],mfun2,siq=siq[i1,i2,i3,],grad=grad,ep=p,control=list(maxit=maxit,reltol=reltol)),
-                 "Jian2"      = optim(c(par[1:(3*k+1)],25),mfun3,siq=siq[i1,i2,i3,],grad=grad,ep=p,control=list(maxit=maxit,reltol=reltol)))
-        rss <- min(z$value,rss)
-        ttt <- z$value+2*(3*k+1)/(ngrad-3*maxcomp-1)*rss
-#        cat("risk",z$value,ttt,"\n")
-        if(ttt < krit) {
-           krit <- ttt
-           order[i1,i2,i3] <- as.integer(k)
-           lev[,i1,i2,i3] <- z$par[1:2]
-           mix[1:k,i1,i2,i3] <- if(k>1) getw(z$par[3*(2:k)-1]) else 1
-           or <- matrix(z$par[rep(3*(1:k),rep(2,k))+c(0,1)],2,k)
-           or[1,or[1,]<0] <- or[1,or[1,]<0]+pi
-           or[1,or[1,]>pi] <- or[1,or[1,]>pi]-pi
-           or[2,or[2,]<0] <- or[2,or[2,]<0]+2*pi
-           or[2,or[2,]>2*pi] <- or[2,or[2,]>2*pi]-2*pi
-           orient[,1:k,i1,i2,i3] <- or
-           if(method=="Jian2") p[i1,i2,i3] <- z$par[length(z$par)]
-       }
-     }
-   sigma2[i1,i2,i3] <- rss/(ngrad-3*mc0-1)
-     }
-   }
- }
+          // estimate models and select
+          rss = R_PosInf;
+          krit = R_PosInf;
+          for (k = mc0; k > 0; k--)
+          {
+            // use log and exp to have free optim problem
+            if (k>1) for (i = 0; i < k-1; i++) par[3*i+4] = -log(k-i-1.); // par[3*(2:k)-1] <- -log((k-1):1)
 
-}*/
+            optimex myoptimpar;
+            myoptimpar.ngrad = ngrad;
+            myoptimpar.siq = siq;
+            myoptimpar.grad = grad;
+
+            switch (*method)
+            { // R code guarantees method is 1, 2, 3
+              case 1:
+                myoptimpar.p = 0; // unused here
+                nmmin(lpar, par, x, &Fmin, fn1,
+                      &fail, R_NegInf, *reltol, &myoptimpar,
+                      1.0, 0.5, 2.0, 0,
+                      &fncount, *maxit);
+              case 2:
+                myoptimpar.p = *ep; // exp for Jian model
+                nmmin(lpar, par, x, &Fmin, fn1,
+                      &fail, R_NegInf, *reltol, &myoptimpar,
+                      1.0, 0.5, 2.0, 0,
+                      &fncount, *maxit);
+//              case 3:
+//                myoptimpar.p = 0; // unused here
+//                nmmin(lpar, par, x, Fmin, fn3,
+//                      &fail, R_NegInf, *reltol, &myoptimpar,
+//                      1.0, 0.5, 2.0, 0,
+//                      &fncount, *maxit);
+            }
+
+            if (Fmin < rss) rss = Fmin;
+            ttt = Fmin + (6.*k+2.)/(ngrad - 3.*mc - 1.) * rss;
+
+            if (ttt < krit)
+            {
+              krit = ttt;
+              order[get_ind3d(i1, i2, i3, n1, n2)] = k;
+              lev[get_ind4d(0, i1, i2, i3, 2, n1, n2)] = x[0];
+              lev[get_ind4d(1, i1, i2, i3, 2, n1, n2)] = x[1];
+              if (k == 1)
+              {
+                mix[get_ind4d(0, i1, i2, i3, mc, n1, n2)] = 1;
+              }
+              else
+              {
+                double zmm[k];  // declare maximum?
+                double zm[k-1]; // declare maximum?
+                for (i = 0; i < k-1; i++) zm[i] = x[3*i-2];
+                getweights(k-1, zm, zmm);
+                for (i = 0; i < k; i++) mix[get_ind4d(i, i1, i2, i3, mc, n1, n2)] = zmm[i];
+              }
+
+              for (i = 0; i < k; i++)
+              {
+                double theta = x[3*i+2];
+                if (theta < 0) theta += M_PI; // define pi!
+                if (theta > M_PI) theta -= M_PI; // define pi!
+                double phi = x[3*i+2];
+                if (phi < 0) phi += M_2PI; // define pi!
+                if (phi > M_2PI) phi -= M_2PI; // define pi!
+                orient[get_ind5d(0, i, i1, i2, i3, 2, mc, n1, n2)] = theta;
+                orient[get_ind5d(1, i, i1, i2, i3, 2, mc, n1, n2)] = phi;
+              }
+//             if (*method == 3) p[i1*n2*n3 + i2*n3 + i3] = x[lpar-1];
+            }
+          }
+          sigma2[get_ind3d(i1, i2, i3, n1, n2)] = rss/(ngrad-3.*mc0-1.);
+        }
+      }
+    }
+  }
+  return;
+}
