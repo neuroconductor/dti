ca9188ba6fd8ed26f5c86966433df4ba863d58ff
stat <stat@8193cdef-c62e-4131-a968-7dcc8d47c1f7>
2011-02-23 12:11:41 +0000

    moved spherharm.r to R removed windoews and unix directories
    Joerg
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/dti-svn/trunk@389 8193cdef-c62e-4131-a968-7dcc8d47c1f7

---
+++ R/spherharm.r
@@ -0,0 +1,67 @@
+getsphericalharmonicseven <- function(order,theta,phi){
+#
+#   compute spherical harmonics
+#
+order <- as.integer(max(0,order))
+if(order%%2==1){
+warning("maximum order needs to be even, increase order by one")
+order <- order+1
+} 
+if(length(theta)!=length(phi)) stop("need same length of theta and phi")
+kseq <- seq(0,order,2)
+n <- length(phi)
+values <- matrix(0,(order+1)*(order+2)/2,n)
+if(require(gsl,quietly = TRUE,warn.conflicts = FALSE)){
+for(k in kseq){
+mseq <- seq(-k,k,1)
+for(m in mseq){
+ind <- (k^2+k+2)/2+m
+z <- legendre_sphPlm(k,abs(m),cos(theta))
+if(m < 0){
+z <- sqrt(2)*z*cos(m*phi)
+} 
+if(m > 0){
+z <- sqrt(2)*z*sin(m*phi)
+}
+values[ind,] <- z
+}
+}
+detach(package:gsl)
+} else {
+warning("gsl package not available \n returning zeros instead of estimates")
+}
+values
+}
+
+getsphericalharmonicsall <- function(order,theta,phi){
+#
+#   compute spherical harmonics
+#
+order <- as.integer(max(0,order))
+if(length(theta)!=length(phi)) stop("need same length of theta and phi")
+kseq <- 0:order
+n <- length(phi)
+values <- matrix(0,(order+1)^2,n)
+if(require(gsl)){
+l <- 1
+for(k in kseq){
+mseq <- (-k):k
+for(m in mseq){
+z <- legendre_sphPlm(k,abs(m),cos(theta))
+if(m < 0){
+z <- sqrt(2)*z*cos(m*phi)
+} 
+if(m > 0){
+z <- sqrt(2)*z*sin(m*phi)
+}
+values[l,] <- z
+l <- l+1
+}
+}
+detach(package:gsl)
+} else {
+warning("gsl package not available \n returning zeros instead of estimates")
+}
+values
+}
+
