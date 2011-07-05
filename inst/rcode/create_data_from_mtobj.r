e9469c135ac21cd33079769bff862bfb46afe1e6
stat <stat@8193cdef-c62e-4131-a968-7dcc8d47c1f7>
2011-07-05 11:17:14 +0000

    corrected code for phantom generation
    Joerg
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/dti-svn/trunk@416 8193cdef-c62e-4131-a968-7dcc8d47c1f7

--- inst/rcode/create_data_from_mtobj.r
+++ inst/rcode/create_data_from_mtobj.r
@@ -1,4 +1,4 @@
-datamixtens <- function(mtobj,grad,sigma,bf=1,smoothS0=TRUE,maxs0=10000){
+datamixtens <- function(mtobj,grad,sigma,bf=1,ns0=NULL,smoothS0=TRUE,maxs0=30000){
 #
 #  this is a mmodification of the function in
 #  source(system.file("rcode/gen_mixtens.r",package="dti"))
@@ -19,8 +19,11 @@ d <- extract(mtobj,"andir")$andir
 th <- mtobj@ev
 order <- mtobj@order
 s0 <- extract(mtobj,"s0")$s0
+if(length(dim(s0))==4) s0 <- apply(s0,1:3,mean)
+ms0 <- median(s0[mtobj@mask])
+if(ms0 < 10000) s0 <- s0/ms0*10000
 ds0 <- dim(s0)
-ns0 <- length(mtobj@s0ind)
+if(is.null(ns0)) ns0 <- length(mtobj@s0ind)
 if(!require(aws)) smoothS0 <- FALSE
 if(smoothS0){
 s0 <- aws(s0,hmax=4)@theta
@@ -28,7 +31,7 @@ s0 <- aws(s0,hmax=4)@theta
 grad <- cbind(matrix(0,3,ns0),grad)
 ngrad <- ngrad + ns0
 dim(s0) <- ds0
-if(max(s0) < maxs0) s0 <- s0/max(s0)*maxs0
+s0[s0>maxs0] <- maxs0
 sb <- array(0,c(dim(s0),ngrad))
 for(i in 1:ns0) sb[,,,i] <- s0
 for(i in (ns0+1):ngrad){
@@ -40,7 +43,9 @@ for(i in (ns0+1):ngrad){
    }
    sb[,,,i] <- sb[,,,i]*s0
 }
-sb <- sqrt(rnorm(sb,sb,sigma)^2+rnorm(sb,0,sigma)^2)
+sb <- as.integer(sqrt(rnorm(sb,sb,sigma)^2+rnorm(sb,0,sigma)^2))
+dim(sb) <- c(prod(ddim),ngrad)
+sb[!mtobj@mask,] <- 0
 dim(sb) <- c(ddim,ngrad)
 ddim0 <- ddim
 invisible(new("dtiData",
@@ -58,8 +63,8 @@ invisible(new("dtiData",
                 zind   = 1:ddim[3],
                 level  = 1,
                 sdcoef = c(5.e-01, 0, 1, 2.e+04),
-                voxelext = c(1,1,1),
-                orientation = as.integer(c(0,2,5)),
+                voxelext = mtobj@voxelext,
+                orientation = mtobj@orientation,
                 rotation = diag(3),
                 source = "artificial")
             )
