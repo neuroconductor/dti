55821176370cf4ca129cedd57268aa02100a6e4f
stat <stat@8193cdef-c62e-4131-a968-7dcc8d47c1f7>
2014-04-11 13:25:23 +0000

    J
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/dti-svn/trunk@645 8193cdef-c62e-4131-a968-7dcc8d47c1f7

--- R/dtineu.r
+++ R/dtineu.r
@@ -272,12 +272,13 @@ dtiTensorChi <- function(object, sigma=NULL, L=1,
    nvox <- sum(mask)
    
    param <- matrix(0,7,nvox)
-   param[1,] <- tensobj@th0[mask]/sigma
+   if(length(sigma)==1) sigma <- array(sigma,ddim)
+   param[1,] <- tensobj@th0[mask]/sigma[mask]
    param[-1,] <- D2Rall(D[,mask]*mbv)
    
    CL <- sqrt(pi/2)*gamma(L+1/2)/gamma(L)/gamma(3/2)
    if(mc.cores==1){
-      si <- t(array(z$si,c(object@ngrad,nvox)))/sigma
+      si <- t(array(z$si,c(object@ngrad,nvox)))/sigma[mask]
       for(i in 1:length(mask)){
          param[,i] <- optim(param[,i],tchi,si=si[,i],btb=btb,L=L,CL=CL,method="BFGS",
                             control=list(reltol=1e-5,maxit=50))$par
@@ -286,14 +287,14 @@ dtiTensorChi <- function(object, sigma=NULL, L=1,
    } else {
       x <- matrix(0,object@ngrad+7,nvox)
       x[1:7,] <- param
-      x[-(1:7),] <- t(t(array(z$si,c(object@ngrad,nvox)))/sigma)
+      x[-(1:7),] <- t(t(array(z$si,c(object@ngrad,nvox)))/sigma[mask])
       param <- plmatrix(x,ptenschi,fn=tchi,btb=btb,L=L,CL=CL)
       cat(nvox,"voxel processed. Time:",format(Sys.time()),"\n")
    }
    D[,mask] <- R2Dall(param[-1,])/mbv
    dim(D) <- c(6,ddim)
    tensobj@D <- D
-   tensobj@th0[mask] <- param[1,]*sigma
+   tensobj@th0[mask] <- param[1,]*sigma[mask]
    tensobj
 }
 ##
