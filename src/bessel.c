6f95b9f40679f0b68666018defd6dc414f37408f
JPolzehl <polzehl@wias-berlin.de>
2019-11-28 18:17:57 +0100

    code no longer needed (moved to aws)

--- src/bessel.c
+++ src/bessel.c
@@ -12,13 +12,3 @@ double F77_SUB(bessliex)(double *x, double *nu, double *expo, double *bi) {
 double F77_SUB(gammaf)(double *x) {
    return gammafn(*x);
 }
-double F77_SUB(digammaf)(double *x) {
-   return digamma(*x);
-}
-double F77_SUB(lgammaf)(double *x) {
-   return lgammafn(*x);
-}
-void F77_SUB(rpsort3)(double *v, int *ii, int *jj)
-{
-    rPsort(v, *ii, *jj);
-}
